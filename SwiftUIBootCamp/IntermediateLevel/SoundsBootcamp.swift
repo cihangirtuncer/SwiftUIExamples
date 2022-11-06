c74b35c4312683d4777cecc5939b3596e09f0e9d
cihangirtuncer <cihangirtuncer184@gmail.com>
2022-11-06 16:24:53 +0300

    [feature] Sounds added

---
+++ SwiftUIBootCamp/IntermediateLevel/SoundsBootcamp.swift
@@ -0,0 +1,69 @@
+//
+//  SoundsBootcamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Cihangir Tuncer on 6.11.2022.
+//
+
+import SwiftUI
+import AVKit
+
+class Soundmanager {
+    
+    static let instance = Soundmanager()
+    
+    var player : AVAudioPlayer?
+    
+    enum SoundOption : String {
+    case tada
+    case badum
+    }
+    
+    func playSound(sound : SoundOption) {
+        guard let url = Bundle.main.url(forResource: sound.rawValue, withExtension: ".mp3") else{return}
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch let error{
+            print("Error playing sound. \(error.localizedDescription)")
+        }
+        
+    }
+}
+
+
+
+struct SoundsBootcamp: View {
+    var body: some View {
+        VStack(spacing: 20){
+            Button {
+                Soundmanager.instance.playSound(sound: .tada)
+            } label: {
+                Text("Tada sounds")
+                    .foregroundColor(Color.white)
+                    .padding()
+                    .background(Color.orange)
+                    .cornerRadius(10)
+                    .shadow(radius: 10)
+            }
+            Button {
+                Soundmanager.instance.playSound(sound: .badum)
+            } label: {
+                Text("Badum sounds")
+                    .foregroundColor(Color.black)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .shadow(radius: 10)
+                    
+            }
+
+        }
+    }
+}
+
+struct SoundsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        SoundsBootcamp()
+    }
+}
