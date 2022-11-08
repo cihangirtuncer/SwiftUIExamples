3a466258832b3003be623b42bccc3efc2e274584
cihangirtuncer <cihangirtuncer184@gmail.com>
2022-11-08 22:06:38 +0300

    [feature] weak self added

--- SwiftUIBootCamp/SwiftUIBootCampApp.swift
+++ SwiftUIBootCamp/SwiftUIBootCampApp.swift
@@ -13,7 +13,7 @@ struct SwiftUIBootCampApp: App {
 
     var body: some Scene {
         WindowGroup {
-            BackgroundThreadBootcamp()
+            WeakSelfBootcamp()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
